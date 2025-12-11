8d0a70e435094b869694e6d14292c0311c50e7fb
TheKingDave <david@langheiter.com>
2025-12-11 22:09:05 +0100

    day 11 - better formatting

--- bin/day_11.dart
+++ bin/day_11.dart
@@ -5,9 +5,12 @@ typedef Devices = Map<String, List<String>>;
 
 void main() async {
   final lines = await getFile().readAsLines();
-  
-  final deviceMap = { for (var parts in lines.map((line) => line.split(': '))) parts[0] : parts[1].split(' ') };
-  
+
+  final deviceMap = {
+    for (var parts in lines.map((line) => line.split(': ')))
+      parts[0]: parts[1].split(' '),
+  };
+
   await part2(deviceMap);
 }
 
@@ -20,36 +23,45 @@ Future<void> part1(Devices deviceMap) async {
     if (dev == null) {
       return 0;
     }
-    
+
     return dev.map(countPaths).sum;
   }
-  
+
   print(countPaths('you'));
 }
 
-typedef Search = (String, bool, bool);
+typedef Search = ({String device, bool dac, bool fft});
+
 Future<void> part2(Devices deviceMap) async {
   final cache = <Search, int>{};
-  
+
   int countPaths(Search search) {
     //print(search);
     final foundCache = cache[search];
     if (foundCache != null) {
       return foundCache;
     }
-    
-    if (search.$1 == 'out') {
-      return search.$2 && search.$3 ? 1 : 0;
+
+    if (search.device == 'out') {
+      return search.dac && search.fft ? 1 : 0;
     }
-    final dev = deviceMap[search.$1];
+    final dev = deviceMap[search.device];
     if (dev == null) {
       return 0;
     }
 
-    final sum = dev.map((d) => countPaths((d, search.$2 || search.$1 == 'dac', search.$3 || search.$1 == 'fft'))).sum;
+    final sum = dev
+        .map(
+          (d) => countPaths((
+            device: d,
+            dac: search.dac || search.device == 'dac',
+            fft: search.fft || search.device == 'fft',
+          )),
+        )
+        .sum;
     cache[search] = sum;
     return sum;
   }
 
-  print(countPaths(('svr', false, false)));
+  print(countPaths((device: 'svr', dac: false, fft: false)));
 }
